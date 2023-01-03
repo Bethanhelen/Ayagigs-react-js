ca6fe66ae8c0de0f40710c19ffe90804c103a261
Bethan Helen <bethanhelen8@gmail.com>
2023-01-03 04:04:38 +0100

    header and hero

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,13 +1,32 @@
 import "./app.css";
+import "./assets/images/hero.jpg";
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        h1 >
-        Hello world <
-        /h1>      <
-        /div >
+    return ( 
+        <div className = "App" >
+            <header>
+                <div className="content-left">
+                    <h1>BETFTs</h1>
+                </div>
+                <div className="content-right">
+                    <a href="#">Home</a>
+                    <a href="#">Explore</a>
+                    <a href="#">Marketplace</a>
+                    <a className='sign' href="#">Sign-up</a>
+                </div>
+            </header>
+
+            <div className="hero">
+                <div className="hero-left">
+                    <h2>Discover, Collect & Sell Popular Nfts</h2>
+                    <p>The world biggest collection of crypto collectible & Non-fungible token</p>
+                    <a className='sign' href="#">Create Nfts</a>
+                </div>
+                <div className="hero-right">
+                    <img src="./assets/images/hero.jpg" alt="" />
+                </div>
+            </div>
+        </div>
     );
 }
 
